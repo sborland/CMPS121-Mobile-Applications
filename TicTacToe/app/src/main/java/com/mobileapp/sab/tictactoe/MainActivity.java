bb3d239f052a3ead92eb9f825fe79064bd0a6de8
sborland <sborland@ucsc.edu>
2016-01-18 15:51:34 -0800

    First Commit

---
+++ TicTacToe/app/src/main/java/com/mobileapp/sab/tictactoe/MainActivity.java
@@ -0,0 +1,71 @@
+package com.mobileapp.sab.tictactoe;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    //buttons
+    Button a1, a2, a3, b1,b2,b3, c1,c2,c3;
+    Button[] bArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Linking buttons to their counterparts in the layout file
+        a1 = (Button) findViewById(R.id.A1);
+        a2 = (Button) findViewById(R.id.A2);
+        a3 = (Button) findViewById(R.id.A3);
+        b1 = (Button) findViewById(R.id.B1);
+        b2 = (Button) findViewById(R.id.B2);
+        b3 = (Button) findViewById(R.id.B3);
+        c1 = (Button) findViewById(R.id.C1);
+        c2 = (Button) findViewById(R.id.C2);
+        c3 = (Button) findViewById(R.id.C3);
+        bArray = new Button[]{a1, a2, a3, b1,b2,b3, c1,c2,c3};
+
+        //event listeners for the buttons
+        for (Button b : bArray){
+            b.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view){
+
+    }
+}
